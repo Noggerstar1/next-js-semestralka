604e40989dcc18433a1b420c582575ea23276e5e
Lukáš Havlíček <lukas.havlic2@gmail.com>
2024-06-04 14:01:28 +0200

    add spacing

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const LinkButton = ({ path, text }: { path: string, text: string }) => {
     return (
-      <Link href={path ?? "/"}>
+      <Link href={path ?? "/"} className="mb-4">
       <div className="text-blue-500 hover:text-blue-700 text-center mx-auto">{text ?? "Home"}</div>
     </Link>
   )
